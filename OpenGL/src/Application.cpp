0563fecafdbf613068adc504e08f2a677ba4e514
motinha122 <timota132@gmail.com>
2022-11-29 08:11:53 -0300

    Inivisible Triangle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -26,8 +26,21 @@ int main(void)
         std::cout << "error" << std::endl;
     }
 
+    /* GL version */
     std::cout << glGetString(GL_VERSION) << std::endl;
-    /* Glew Test */
+
+    /* Triangle Positions */
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f,
+    }; // each index is a vertex, theres 3 indexes here 
+
+    /* Buffer */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -35,11 +48,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0 , 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
